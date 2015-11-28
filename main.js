41d48cfad6c7def40790a9cc22fa3fbdf15222df
Bai Sijian <sijian.bai@rea-group.com>
2015-11-28 20:30:01 +0800

    21

--- main.js
+++ main.js
@@ -9,6 +9,24 @@ app.use(morgan('dev'));
 app.listen(process.env.PORT || 3000);
 console.log('Node.js Ajax Upload File running at: http://0.0.0.0:3000');
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.post('/upload', function(req, res, next) {
   //get filename
@@ -27,6 +45,7 @@ app.get('/', function(req, res) {
         res.send(text);
     });
 });
+
 
 app.get('/env', function(req, res){
   console.log("process.env.VCAP_SERVICES: ", process.env.VCAP_SERVICES);
